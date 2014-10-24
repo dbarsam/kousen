6b80762a6d8e9f1ac491bc5cc234b434f70f724a
dbarsam <dbarsam@gmail.com>
2014-10-24 00:05:32 -0400

    Added initial view module.

---
+++ kousen/ui/views.py
@@ -0,0 +1,25 @@
+from PySide import QtGui, QtCore
+
+class TreeView(QtGui.QTreeView):
+    """!
+    TreeView extends the PySide QtGui.QTreeView object.
+    """
+    currentSelectionChanged = QtCore.Signal(object, object)
+
+    def __init__(self, parent=None):
+        """
+        Constructor
+        """
+        super(TreeView, self).__init__(parent)
+
+    def selectionChanged(self, selected, deselected):
+        """
+        Override the QtGui.QTreeView selectionChanged() method.
+
+        @param selected   The new selection (which may be empty)
+        @param deselected The previous selection (which may be empty)
+        """        
+        self.currentSelectionChanged.emit(selected, deselected)
+        
+        super(TreeView, self).selectionChanged(selected, deselected)
+ 
