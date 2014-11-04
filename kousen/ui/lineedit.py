061c9431e027d13383dcb6d350205636c999955b
dbarsam <dbarsam@gmail.com>
2014-11-03 23:09:53 -0500

    Renamed LineFilter to FilterLineEdit.

--- kousen/ui/lineedit.py
+++ kousen/ui/lineedit.py
@@ -1,27 +1,28 @@
 from PySide import QtGui, QtCore
 
-class LineFilter(QtGui.QLineEdit):
-    """A QLineEdit derived class"""
-    
+class FilterLineEdit(QtGui.QLineEdit):
+    """
+    The FilterLineEdit class provides a modified QLineEdit with a filter state icon.
+    """
     activated = QtCore.Signal(bool)
 
     def __init__(self, parent=None):
         """
         Constructor.
         """
-        super(LineFilter, self).__init__(parent)
+        super(FilterLineEdit, self).__init__(parent)
 
-        self.__searchPixmap = QtGui.QPixmap(":/icons/search.png")
-        self.__cancelPixmap = QtGui.QPixmap(":/icons/cancel.png")
+        self._searchPixmap = QtGui.QPixmap(":/icons/search.png")
+        self._cancelPixmap = QtGui.QPixmap(":/icons/cancel.png")
 
-        self.__button = QtGui.QToolButton(self)
-        self.__button.setIcon(QtGui.QIcon(self.__searchPixmap))
-        self.__button.setCursor(QtCore.Qt.ArrowCursor)
-        self.__button.setStyleSheet("QToolButton { background-color: transparent; border: none; padding: 0px; }")
-        self.__button.show()
-        self.__button.setEnabled(False)
+        self._button = QtGui.QToolButton(self)
+        self._button.setIcon(QtGui.QIcon(self._searchPixmap))
+        self._button.setCursor(QtCore.Qt.ArrowCursor)
+        self._button.setStyleSheet("QToolButton { background-color: transparent; border: none; padding: 0px; }")
+        self._button.show()
+        self._button.setEnabled(False)
 
-        self.__button.clicked.connect(self.clear)
+        self._button.clicked.connect(self.clear)
         self.textChanged.connect(self.updateState)
 
         self.setProperty("isActive", False)
@@ -30,20 +31,20 @@ class LineFilter(QtGui.QLineEdit):
         self.update()
 
         frameWidth = self.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
-        self.setStyleSheet("padding-right: {0}px".format(self.__button.sizeHint().width() + frameWidth + 1))
+        self.setStyleSheet("padding-right: {0}px".format(self._button.sizeHint().width() + frameWidth + 1))
 
         minSize = self.minimumSizeHint()
-        self.setMinimumSize(max(minSize.width(), self.__button.sizeHint().width() + frameWidth * 2 + 2),
-                            max(minSize.height(), self.__button.sizeHint().height() + frameWidth * 2 + 2))
+        self.setMinimumSize(max(minSize.width(), self._button.sizeHint().width() + frameWidth * 2 + 2),
+                            max(minSize.height(), self._button.sizeHint().height() + frameWidth * 2 + 2))
 
     def active(self):
         return self.property("isActive")
 
     def resizeEvent(self, event):
-        super(LineFilter, self).resizeEvent(event)
-        buttonSize = self.__button.sizeHint()
+        super(FilterLineEdit, self).resizeEvent(event)
+        buttonSize = self._button.sizeHint()
         frameWidth = self.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
-        self.__button.move(self.rect().right() - frameWidth - buttonSize.width(), (self.rect().bottom() + 1 - buttonSize.height())/2)
+        self._button.move(self.rect().right() - frameWidth - buttonSize.width(), (self.rect().bottom() + 1 - buttonSize.height())/2)
 
     def updateState(self, text):
         active = self.property("isActive")
@@ -51,13 +52,31 @@ class LineFilter(QtGui.QLineEdit):
             active = bool(text)
             self.setProperty("isActive", active)
             if active:
-                self.__button.setIcon(QtGui.QIcon(self.__cancelPixmap))
+                self._button.setIcon(QtGui.QIcon(self._cancelPixmap))
             else:
-                self.__button.setIcon(QtGui.QIcon(self.__searchPixmap))
+                self._button.setIcon(QtGui.QIcon(self._searchPixmap))
             self.activated.emit(active)
-            self.__button.setEnabled(active)
+            self._button.setEnabled(active)
             self.setProperty("isActive", active)
             self.style().unpolish(self)        
             self.style().polish(self)
             self.update()
         
+
+if __name__ == "__main__":
+    import sys
+    import os
+    
+    packagepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+    if not packagepath in sys.path:
+        sys.path.append(packagepath)
+
+    import kousen.resources_rc
+
+    app = QtGui.QApplication.instance()
+    if not app: app = QtGui.QApplication([])
+
+    filter = FilterLineEdit(None)
+    filter.show()
+
+    app.exec_()
