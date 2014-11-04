69312f88c3646b6bc8f6fe519d810cc39b3eb449
dbarsam <dbarsam@gmail.com>
2014-11-03 23:13:14 -0500

    Added ColorLineEdit to lineedit.

--- kousen/ui/lineedit.py
+++ kousen/ui/lineedit.py
@@ -1,5 +1,91 @@
 from PySide import QtGui, QtCore
 
+class ColorLineEdit(QtGui.QLineEdit):
+    """
+    The ColorLineEdit class provides a modified QLineEdit with a Color decoration and a 'browse' button to launch a QtGui.QColorDialog.
+    """
+    colorChanged = QtCore.Signal(QtGui.QColor)    
+    colorRegex   = QtCore.QRegExp('^#[A-Fa-f0-9]{6}')
+
+    def __init__(self, parent=None):
+        """
+        Constructor
+        """
+        super(ColorLineEdit, self).__init__(parent)
+
+        self._lbutton = QtGui.QToolButton(self)        
+        self._lbutton.setCursor(QtCore.Qt.ArrowCursor)
+        self._lbutton.setSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Maximum)
+        self._lbutton.setStyleSheet("QToolButton { background-color: transparent; border: none; padding: 0px;}")
+
+        self._rbutton = QtGui.QToolButton(self)
+        self._rbutton.setText('...')
+        self._rbutton.setSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Maximum)
+        self._rbutton.setCursor(QtCore.Qt.ArrowCursor)
+        self._rbutton.clicked.connect(self.showDialog)
+
+        frameWidth = self.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
+        rbuttonSize = self._rbutton.sizeHint()
+        lbuttonSize = self._lbutton.sizeHint()
+        self.setStyleSheet('QLineEdit {padding-left: %dpx; padding-right: %dpx; }' % (lbuttonSize.width() + frameWidth + 1, rbuttonSize.width() + frameWidth + 1))
+        self.setMinimumSize(max(self.minimumSizeHint().width(), lbuttonSize.width() + rbuttonSize.width() + frameWidth*2 + 2), self.minimumSizeHint().height())
+
+        self.setValidator(QtGui.QRegExpValidator(ColorLineEdit.colorRegex, self))
+        
+        self.editingFinished.connect(lambda: self.setColor(QtGui.QColor(self.text())))
+
+        self.setColor(QtGui.QColor(0,0,0))
+
+    def resizeEvent(self, event):
+        """
+        Overrides the QtGui.QWidget resizeEvent method for custom resize handling.
+
+        @param event A QResizeEvent containing the resize parameters.
+        """
+        super(ColorLineEdit, self).resizeEvent(event)
+        
+        frameWidth = self.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
+        
+        rbuttonSize = QtCore.QSize(self._rbutton.sizeHint().width(), (self.height() - frameWidth * 2))        
+        self._rbutton.setFixedSize(rbuttonSize)
+        self._rbutton.move(self.rect().right() - frameWidth - rbuttonSize.width(), (self.rect().bottom() + 1 - rbuttonSize.height())/2)
+
+        lbuttonSize = QtCore.QSize(self._lbutton.sizeHint().width(), (self.height() - frameWidth * 2))                
+        self._lbutton.setFixedSize(lbuttonSize)
+        self._lbutton.move(self.rect().left() + frameWidth + 1, self.rect().top() + frameWidth)
+        
+    def showDialog(self):
+        """
+        Launches the Color Dialog and stores the resulting color, if valid.
+        """
+        color = QtGui.QColorDialog.getColor(self.getColor(), self)
+        if color.isValid():
+            self.setColor(color)
+
+    def getColor(self):
+        """
+        Gets the current value color.
+
+        @return A valid QtGui.QColor instance.
+        """
+        return self._color
+
+    def setColor(self, color):
+        """
+        Sets the current value color.
+
+        @param color A valid QtGui.QColor instance.
+        """
+        self._color = color
+        
+        pixmap = QtGui.QPixmap(5,5)
+        pixmap.fill(color)
+        self._lbutton.setIcon(QtGui.QIcon(pixmap.scaled(self._lbutton.sizeHint(), QtCore.Qt.KeepAspectRatio)))
+        
+        self.setText(color.name())
+
+        self.colorChanged.emit(self._color)
+
 class FilterLineEdit(QtGui.QLineEdit):
     """
     The FilterLineEdit class provides a modified QLineEdit with a filter state icon.
@@ -76,7 +162,10 @@ if __name__ == "__main__":
     app = QtGui.QApplication.instance()
     if not app: app = QtGui.QApplication([])
 
-    filter = FilterLineEdit(None)
-    filter.show()
+    filterline = FilterLineEdit(None)
+    filterline.show()
+
+    colorline = ColorLineEdit(None)
+    colorline.show()
 
     app.exec_()
